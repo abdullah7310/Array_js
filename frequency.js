85545d1d83850047c810bf59390c45331ebb48b2
Abdullah Ahmad <abdul7310ahmad@gmail.com>
2024-07-08 10:36:28 +0530

    added new files

---
+++ frequency.js
@@ -0,0 +1,46 @@
+const prompt = require('prompt-sync')()
+// let n = Number(prompt("enter the number (n) >> "))
+// let arr = []
+// for (let i = 0; i < n; i++) {
+//     let element = Number(prompt(`enter the element ${i + 1} >> `))
+//     arr.push(element)
+// } console.log(arr);
+// // let arr1;
+// for (let i = 0; i < arr.length; i++) {
+    
+//     if (arr[i] !== null) {
+//         let count = 1;
+//         for (let j = i + 1; j < arr.length; j++) {
+            
+//             if (arr[i] === arr[j]) {
+//                 count += 1;
+//                 arr[j]=null
+
+//             }
+//         }console.log(`Frequency of ${arr[i]}  is ${count}`);
+//     }            
+
+
+
+// }
+
+
+// let n = Number(prompt("enter the number (n) >> "))
+  
+// let arr = []
+// for(let i =0 ;i<n;i++){
+//     let element = Number(prompt(`enter the element ${i+1} >> `))
+//     arr.push(element)
+// }console.log(arr);
+// for(let i = 0;i<arr.length;i++){
+//     for(let j = 0;j<arr.length;j++){
+//         if(arr[j]>arr[j+1]){
+//             let temp;
+//             temp = arr[j]
+//             arr[j]=arr[j+1]
+//             arr[j+1]=temp
+//         }
+//     }
+// }console.log(arr);
+
+
